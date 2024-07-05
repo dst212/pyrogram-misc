b61ec8be2b22520e6e813e3dde6cab152c9a4a44
dst212 <212dst212@gmail.com>
2024-07-05 19:40:04 +0200

    Added verbose mode (/ping v)
    It actually shows no more than the latency but that's all we need.

--- commands/ping.py
+++ commands/ping.py
@@ -1,7 +1,13 @@
+import time
 from pyrogram import filters
 
 
 def init(bot, name: str = "ping"):
     @bot.on_message(filters.command(name))
     async def _(bot, m):
-        await m.reply("Pong")
+        if len(m.command) == 1:
+            await m.reply("Pong")
+        else:
+            start = time.time()
+            r = await m.reply("Pong")
+            await r.edit(f"Pong ({time.time() - start:.3f}s)")
